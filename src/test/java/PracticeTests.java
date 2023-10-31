46f6f07ca81eaae2dfcf299d6d57f210f31718d5
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-10-31 21:22:08 +0300

    Правки6

--- src/test/java/PracticeTests.java
+++ src/test/java/PracticeTests.java
@@ -4,13 +4,16 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+
 public class PracticeTests {
+
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
     }
+
     @Test
     void qaguruHWGIT() {
         open("/selenide/selenide");
